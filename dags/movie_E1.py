a81878fee7ac983dc84912989a7ee9a812345033
Mingk42 <xoals123456t@gmail.com>
2024-08-05 14:40:41 +0900

    E1 modified

--- dags/movie_E1.py
+++ dags/movie_E1.py
@@ -19,7 +19,7 @@ with DAG(
     description="extract movie rank 2018.01~04.",
     schedule="10 0 * * *",
     start_date=datetime(2018, 1, 1),
-    end_date=datetime(2018, 1, 30),
+    end_date=datetime(2018, 4, 30),
     catchup=True,
     tags=["movie","E", "extract","2018"],
 ) as dag:
@@ -32,11 +32,37 @@ with DAG(
 
     def get_data(ds_nodash,url_param={}):
         from extract.extract import save2df
+        import pandas as pd
 
         df=save2df(ds_nodash,url_param)
 
-        print(df)
-    
+        df.to_parquet("~/code/de32-kca/data_kca/",partition_cols=["load_dt","repNationCd"])
+
+    def chk_exist(ds_nodash="20180101"):
+        import os
+        
+        home_dir = os.path.expanduser("~")
+        target_path=f"{home_dir}/code/de32-kca/data_kca/load_dt={ds_nodash}"
+        
+        print( os.path.exists(target_path),target_path)
+
+        if os.path.exists(target_path):
+            return "rm.dir"
+        else:
+            return "get.start"
+
+    task_chk_exist=BranchPythonOperator(
+                task_id="chk.exist",
+                python_callable=chk_exist,
+            )
+
+    task_rm_dir=BashOperator(
+                task_id="rm.dir",
+                bash_command="""
+                    rm -rf ~/code/de32-kca/data_kca/load_dt={{ds_nodash}}
+                """
+                )
+
     task_get_data=PythonVirtualenvOperator(
                 task_id="get.data",
                 python_callable=get_data,
@@ -54,7 +80,7 @@ with DAG(
                     "url_param":{ "repNationCd":"K"}
                 }
             )
-
+    """
     task_save_data=PythonVirtualenvOperator(
                 task_id="save.data",
                 python_callable=pic,
@@ -62,7 +88,7 @@ with DAG(
                 system_site_packages=False,
                 # op_kwargs=kwargs["op_kwargs"]
             )
-
+    """
     task_start=PythonVirtualenvOperator(
                 task_id="start",
                 python_callable=pic,
@@ -79,4 +105,23 @@ with DAG(
                 # op_kwargs=kwargs["op_kwargs"]
             )
 
-    task_start >> [task_get_data, task_get_data_kor] >> task_save_data >> task_end
+    task_get_start=PythonVirtualenvOperator(
+                task_id="get.start",
+                python_callable=pic,
+                requirements=["git+https://github.com/de32-kca/extract.git@d2.0.0/mingk"],
+                system_site_packages=False,
+                # op_kwargs=kwargs["op_kwargs"],
+                trigger_rule="none_failed"
+            )
+    task_get_end=PythonVirtualenvOperator(
+                task_id="get.end",
+                python_callable=pic,
+                requirements=["git+https://github.com/de32-kca/extract.git@d2.0.0/mingk"],
+                system_site_packages=False,
+                # op_kwargs=kwargs["op_kwargs"],
+                trigger_rule="none_failed"
+
+            )
+    task_start >> task_chk_exist >> [task_rm_dir, task_get_start]
+    task_rm_dir >> task_get_start
+    task_get_start >> [task_get_data, task_get_data_kor] >> task_get_end >> task_end
